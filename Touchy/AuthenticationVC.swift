15f3590d840eed148038a154a576c1c65d4a0789
Fabio Cipriani <FabioCipriani@mbp-di-fabio.station>
2016-09-10 06:19:18 +0200

    Implemented functions to manage the start of authentication through TouchID sensor, handling errors accordingly to the fact that the sensor may not be present, of the authentication may be canceled by the user in various ways, via the cancel button, the press ion of the home button, or the authentication was not successful.

--- Touchy/AuthenticationVC.swift
+++ Touchy/AuthenticationVC.swift
@@ -7,19 +7,95 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class AuthenticationVC: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBAction func useTouchIDBtn(_ sender: RoundedOutlineButton) {
+        let authenticationContext = LAContext()
+        var error: NSError?
+        
+        if authenticationContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            // Show touchID prompt, navigate to SuccessVC
+            authenticationContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Only humans allowed. No dogs! 🐶", reply: { (success, error) in
+                if success {
+                    // Navigate to successVC
+                    self.navigateToAuthenticatedVC()
+                } else {
+                    if let err = error as? NSError {
+                        // Display an error of a specific type
+                        let message = self.errorMessageForLAErrorCode(errorCode: err.code)
+                        self.showAlertViewAfterEvaluatingPolicyWithMessage(message: message)
+                    }
+                }
+            })
+        } else {
+            showAlertViewForNoBiometrics()
+            return
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func showAlertViewAfterEvaluatingPolicyWithMessage(message: String) {
+        showAlertWithTitle(title: "Error", message: message)
     }
-
-
+    
+    private func errorMessageForLAErrorCode(errorCode: Int) -> String {
+        var message = ""
+        switch errorCode {
+        case LAError.appCancel.rawValue:
+            message = "Authentication was cancelled by application"
+            
+        case LAError.authenticationFailed.rawValue:
+            message = "The user failed to provide valid credentials"
+            
+        case LAError.invalidContext.rawValue:
+            message = "The context is invalid"
+            
+        case LAError.passcodeNotSet.rawValue:
+            message = "Passcode is not set on the device"
+            
+        case LAError.systemCancel.rawValue:
+            message = "Authentication was cancelled by the system"
+            
+        case LAError.touchIDLockout.rawValue:
+            message = "Too many failed attempts."
+            
+        case LAError.touchIDNotAvailable.rawValue:
+            message = "TouchID is not available on the device"
+            
+        case LAError.userCancel.rawValue:
+            message = "The user did cancel"
+            
+        case LAError.userFallback.rawValue:
+            message = "The user chose to use the fallback"
+            
+        default:
+            message = "Did not find error code on LAError object"
+        }
+        return message
+    }
+    
+    private func navigateToAuthenticatedVC() {
+        if let loggedInVC = storyboard?.instantiateViewController(withIdentifier: "LoggedInVC") {
+            DispatchQueue.main.async {
+                self.navigationController?.pushViewController(loggedInVC, animated: true)
+            }
+        }
+    }
+    
+    private func showAlertViewForNoBiometrics() {
+        showAlertWithTitle(title: "Error", message: "Your device has no TouchID sensor")
+    }
+    
+    private func showAlertWithTitle(title: String, message: String) {
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertVC.addAction(okAction)
+        DispatchQueue.main.async {
+            self.present(alertVC, animated: true, completion: nil)
+        }
+    }
 }
 
